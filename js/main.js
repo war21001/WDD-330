1d5cf3b13dd89e9fcd068b061dc48b013a793ec7
war21001 <83742731+war21001@users.noreply.github.com>
2021-10-15 09:24:19 -0600

    comma

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function showNav(){
         {
           label: "Week 5 Notes",
           url: "Week5/week5.html"
-        }
+        },
     ];    
     //finds the ordered list in the index page
     const ul= document.querySelector("ul");
