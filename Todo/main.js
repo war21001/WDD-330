ffd64bce4b9b9564f463b8344f04d2c26b764099
war21001 <83742731+war21001@users.noreply.github.com>
2021-10-15 20:58:56 -0600

    some more stuff

--- Todo/main.js
+++ Todo/main.js
@@ -1,23 +1,24 @@
 import ToDo from "./todo.js";
 
-var listCnt=0;
+// create toDoList
 var toDoList = [];
-toDoList[listCnt] = new ToDo("0", "laundry", false, false);
+
+
+toDoList[toDoList.length++] = new ToDo("0", "laundry", false, false);
 // toDoList[listCnt].addTodo();
 // console.log(listCnt);
 // toDoList[listCnt].getTodos();
-toDoList[listCnt].markComplete();
+toDoList[0].markComplete();
 // toDoList[listCnt].getTodos();
-listCnt++;
-toDoList[listCnt] = new ToDo("1", "dishes", false, false);
+
+toDoList[toDoList.length++] = new ToDo("1", "dishes", false, false);
 // toDoList[listCnt].addTodo();
 // toDoList[listCnt].getTodos();
 // toDoList[listCnt].markComplete();
 // // toDoList[listCnt].getTodos();
 toDoList[1].deleteToDo();
 
-listCnt++;
-toDoList[listCnt] = new ToDo("2", "dust", false, false);
+toDoList[toDoList.length++] = new ToDo("2", "dust", false, false);
 
 for(var i=0;i<toDoList.length;i++){
     toDoList[i].getTodos();
--- Todo/utilities.js
+++ Todo/utilities.js
@@ -8,3 +8,5 @@ function onTouch(elementSelector, callback){
     // add a touchend event listener to an element for mobile with a click event fallback for desktops @param {string} elementSelector The selector for the element to attach the listener to
     // * @param {function} callback The callback function to run
 }
+
+f
