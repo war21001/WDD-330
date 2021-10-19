a7497d9c080139e90050142c4574c4759b9e79ff
war21001 <83742731+war21001@users.noreply.github.com>
2021-10-18 20:06:29 -0600

    major update

--- Todo/main.js
+++ Todo/main.js
@@ -1,30 +1,114 @@
 'use strict;'
-import ToDo from "./todo.js";
 
-// create toDoList
-var toDoList = [];
-
-
-toDoList[toDoList.length++] = new ToDo("0", "laundry", false, false);
-// toDoList[listCnt].addTodo();
-// console.log(listCnt);
-// toDoList[listCnt].getTodos();
-toDoList[0].markComplete();
-// toDoList[listCnt].getTodos();
-
-toDoList[toDoList.length++] = new ToDo("1", "dishes", false, false);
-// toDoList[listCnt].addTodo();
-// toDoList[listCnt].getTodos();
-// toDoList[listCnt].markComplete();
-// // toDoList[listCnt].getTodos();
-toDoList[1].deleteToDo();
-
-toDoList[toDoList.length++] = new ToDo("2", "dust", false, false);
-
-for(var i=0;i<toDoList.length;i++){
-    toDoList[i].getTodos();
+var myTaskList = document.getElementsByTagName("LI");
+//add a button to task item
+for (var i = 0; i < myTaskList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myTaskList[i].appendChild(span);
 }
+var close = document.getElementsByClassName("close");
 
-// toDoList[1].getTodos();
-// toDoList[2].getTodos();
+for ( var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
+// import ToDo from "./todo.js";
+
+// // create toDoList
+// var toDoList = [];
+
+
+// toDoList[toDoList.length++] = new ToDo("0", "laundry", false, false);
+// // toDoList[listCnt].addTodo();
+// // console.log(listCnt);
+// // toDoList[listCnt].getTodos();
+// toDoList[0].markComplete();
+// // toDoList[listCnt].getTodos();
+
+// toDoList[toDoList.length++] = new ToDo("1", "dishes", false, false);
+// // toDoList[listCnt].addTodo();
+// // toDoList[listCnt].getTodos();
+// // toDoList[listCnt].markComplete();
+// // // toDoList[listCnt].getTodos();
+// toDoList[1].deleteToDo();
+
+// toDoList[toDoList.length++] = new ToDo("2", "dust", false, false);
+
+// for(var i=0;i<toDoList.length;i++){
+//     toDoList[i].getTodos();
+// }
+
+// // toDoList[1].getTodos();
+// // toDoList[2].getTodos();
+
+// // function addNewTask(){
+// //     var li = document.createElement("li");
+// //     var inputValue = document.getElementById("myInput")
+
+// // }
+
+
+function addNewTask(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("newTask").value;
+    var task = document.createTextNode(inputValue);
+    li.appendChild(task);
+    
+    if(inputValue === ''){
+        alert("you must write something!");
+    }else{
+        document.getElementById("taskList").appendChild(li);
+    }
+
+    document.getElementById("newTask").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode(`\u00D7`);
+    span.className="close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    console.log(close.length);
+
+    for (var i=0; i < close.length; i++){
+        close[i].onclick=function(){
+            var div = this.parentElement;
+            div.style.display="none";
+            
+        }
+    }
+  }
+  
+  function toggleView(view){
+    console.log("made it");
+    var viewAll = document.getElementById("all");
+    var viewActive = document.getElementById("active");
+    var viewCompleted = document.getElementById("completed");
+
+    if (view === 'all'){
+        viewCompleted.style.display= "none";
+        viewActive.style.display= "none";
+        viewAll.style.display="block";
+    }
+    if(view === 'active'){
+        viewActive.style.display="block";
+        viewAll.style.display="none";
+        viewCompleted.style.display= "none";    
+    }
+    if(view === 'completed'){
+        viewActive.style.display="none";
+        viewAll.style.display="none";
+        viewCompleted.style.display= "block";   
+    }
+  }
--- Todo/utilities.js
+++ Todo/utilities.js
@@ -1,11 +1,74 @@
-function qs(selector){
-// do a querySelector lookup @param {string} selector The selector passed to querySelector
-// @return {element} The matching element or null if not found 
 
+var myNodelist = document.getElementsByTagName("li");
+//add a button to task item
+for (var i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
-
-function onTouch(elementSelector, callback){
-    // add a touchend event listener to an element for mobile with a click event fallback for desktops @param {string} elementSelector The selector for the element to attach the listener to
-    // * @param {function} callback The callback function to run
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
+function toggleView(view){
+    console.log("made it");
+    var viewAll = document.getElementById("all");
+    var viewActive = document.getElementById("active");
+    var viewCompleted = document.getElementById("completed");
+
+    if (view === 'all'){
+        viewCompleted.style.display= "none";
+        viewActive.style.display= "none";
+        viewAll.style.display="block";
+    }
+    if(view === 'active'){
+        viewActive.style.display="block";
+        viewAll.style.display="none";
+        viewCompleted.style.display= "none";    
+    }
+    if(view === 'completed'){
+        viewActive.style.display="none";
+        viewAll.style.display="none";
+        viewCompleted.style.display= "block";   
+    }
+  }
+
+  function addNewTask(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("newTask").value;
+    var task = document.createTextNode(inputValue);
+    li.appendChild(task);
+    if(inputValue === ''){
+        alert("you must write something!");
+    }else{
+        document.getElementById("taskList").appendChild(li);
+    }
+    document.getElementById("newTask").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode(`\u00D7`);
+    span.classname="close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i=0; i<close.length;I++){
+        close[i].onclick=function(){
+            var div = this.parentElement;
+            div.style.display="none";
+        }
+    }
+  }
+  
