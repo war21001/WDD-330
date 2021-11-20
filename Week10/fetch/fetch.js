c45a49d778ce9078f2618f4cda821ee066328d9c
war21001 <83742731+war21001@users.noreply.github.com>
2021-11-20 16:29:05 -0700

    fetch example

---
+++ Week10/fetch/fetch.js
@@ -0,0 +1,23 @@
+'use strict;'
+
+function handleResponse(json){
+   const movies=json.movie_results;
+    // console.log('this is the json data', movies);
+    var list='<ul>';
+    for(const movie in movies){
+        // console.log(`${movie}: ${movies[movie].title}`);
+        list += `<li>${movies[movie].title}</li>`;
+    }
+    list += `</ul>`;
+    document.getElementById("movie_list").innerHTML=list;
+}
+
+fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-byyear&page=1&year=1977", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+		"x-rapidapi-key": "986156c66bmsh1dc94318b470e23p1c6b23jsn0ec68d8c96eb"
+	}
+})
+.then(response => response.json())
+.then(handleResponse)   
