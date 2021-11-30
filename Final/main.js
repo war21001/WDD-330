fd1061115d2dbd193c58fdf0ad7e6efe7609e05e
war21001 <83742731+war21001@users.noreply.github.com>
2021-11-30 15:13:52 -0700

    work 11-30-21

--- Final/main.js
+++ Final/main.js
@@ -1,5 +1,16 @@
 "use strict;"
 
+// pagination code from developer.edamam.com
+// "_links" : {
+//     "next" : {
+//       "title" : "Next page",
+//       "href" : "https://api.edamam.com/api/food-database/v2/parser?..."
+//     }
+// }
+
+var recipeList = [];
+
+
 function clickButton(){
     let ingredient = document.getElementById("ingredient").value;
     let url="https://edamam-recipe-search.p.rapidapi.com/search?q=" + ingredient;
@@ -12,12 +23,7 @@ function clickButton(){
             "x-rapidapi-key": "986156c66bmsh1dc94318b470e23p1c6b23jsn0ec68d8c96eb"
         }
     })
-    // .then(response => {
-    //     console.log(response);
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    // });
+    
     .then(response => response.json())
     .then(handleResponse)   
    
@@ -31,16 +37,39 @@ function clickButton(){
 }
 
 function handleResponse(json){
-    const recipeList=json.hits;
+     recipeList=json.hits;
      console.log('this is the json data', recipeList);
      var list='<ul>';
      for(const rec in recipeList){
        
-         list += `<li><a href="#">${recipeList[rec].recipe.label}</a></li>`;
+         list += `<li><a href="#" class="recipeLink">${recipeList[rec].recipe.label}</a></li>`;
      }
      list += `</ul>`;
      document.getElementById("recipe-list").innerHTML = list;
  }
 
-
+document.querySelector("body").addEventListener('click', function(e) {
+    // console.log("made it here");
+    var anchor = e.target.closest('a').text;
+    // console.log(anchor);
+    if(anchor !==null){
+        for(const rec in recipeList){
+            if(anchor === recipeList[rec].recipe.label){  
+                document.getElementById("recipe-title").style.display="block";
+                document.getElementById("recipe-title").innerHTML=`<h2>${recipeList[rec].recipe.label}</h2>`;
+                document.getElementById("recipe").style.display="block";
+                var list='<ul>';
+                for(const ingredient in recipeList[rec].recipe.ingredientLines){
+                    list += `<li>${recipeList[rec].recipe.ingredientLines[ingredient]}</li>`;
+                }
+                list += `</ul>`;
+                list += `<p>Get the instructions <a href=${recipeList[rec].recipe.url}>Here</a></p>`;
+                document.getElementById("recipe").innerHTML = list;
+                
+                document.getElementById("picture").style.display="block";
+                document.getElementById("picture").innerHTML=`<img src=${recipeList[rec].recipe.image} alt="picture from edmam site">`;
+            }
+        }
+    }
+}, false);
 
